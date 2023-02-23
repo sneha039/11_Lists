4eb42c3b69d58e04d95e42b25c5e9b8b736fd0ee
sneha039 <123440006+sneha039@users.noreply.github.com>
2023-02-23 15:39:39 +0530

    Add files via upload

---
+++ Datatype_Code.py
@@ -0,0 +1,53 @@
+'''
+Day: Tuesday
+Date: 24th Jna 2023
+Topic: Data Types(List, Tuples)
+Description: List and Tuples are the data types in python
+List: It is a data type which is enclosed in square brackets and Mutable.
+Tuple:It is a data type in which multiple values can be assinged to a single variable which is enclosed in paranthesis and Immutable.
+'''
+
+'''None Type'''
+# a=None
+# print(type(a))
+
+'''List Data Type'''
+
+# b=[0,4.6,[7,8.5],"hi"]
+# print(type(b))
+# print(b)
+# print(b[3],type(b[3]))
+# print(b[0],type(b[0]))
+# print(b[1],type(b[1]))
+# print(b[2],type(b[2]))
+# print(b[4]) # Error
+# print(b[2][0],type(b[2][0]))
+# print(b[2][1],type(b[2][1]))
+# c=[]
+# print(c,type(c))
+
+'''Changing the values in List'''
+
+# v=[3,6]
+# v[0]=8
+# print(v)
+
+'''Tuple Data Type'''
+
+# a=3,4,5,6
+# print(a,type(a))
+
+# a,b,c=2,3,6
+# print(type(a))
+# print(type(a),type(b),type(c))
+
+# b=4,7.9,'hi',[3,2],(6,7),None
+# print(b,type(b))
+# print(type(b[0]))
+# print(type(b[3]))
+# print(type(b[4]))
+
+'''Changing Value in Tuple'''
+# v=(3,6)
+# print(type(v))
+# v[0]=8  # TypeError: does not support item assignment
